9f3a5118c2b5acec815270292c090e678e8db9a0
esazjue <juan.j.sanchez@gmail.com>
2016-07-03 20:18:12 +0200

    Create run_analysis.R
    Initial version

---
+++ run_analysis.R
@@ -0,0 +1,3 @@
+#Loading features and activity labels files common to test and train data
+features <- read.table("./UCI HAR Dataset/features.txt") 
+activity_labels <- read.table("./UCI HAR Dataset/activity_labels.txt") 
